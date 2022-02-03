f895bab492e6788b071a7f43705bdc8c9759db94
aguilarkevin <kevinaguilar0813@gmail.com>
2022-02-02 19:20:01 -0600

    wip: custom theme settings

---
+++ theme/foundations/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  brand: {
+    bgGray: `#111111`,
+    textGray: `#626262`,
+    orange: `#CB450C`
+  }
+};
+
+export default colors;
---
+++ theme/foundations/sizes.ts
@@ -0,0 +1,16 @@
+const container = {
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px"
+};
+
+const sizes = {
+  "30": "7.5rem",
+  "h-screen": "100vh",
+  "w-screen": "100vw",
+  container
+};
+
+export default sizes;
