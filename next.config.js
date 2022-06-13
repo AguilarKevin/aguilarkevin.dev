a9b1282064c92a3fb41101c7f760c2858725fb2d
aguilarkevin <kevinaguilar0813@gmail.com>
2022-06-12 19:36:26 -0600

    chore: :wrench: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: [
+      'components',
+      'context',
+      'pages',
+      'relay',
+      'sections',
+      'theme',
+      'utils',
+    ],
+  },
+  experimental: {
+    reactRoot: true,
+  },
   reactStrictMode: true,
 }
 
