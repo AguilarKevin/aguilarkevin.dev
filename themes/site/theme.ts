f2fd4d0de20c521996ef1d88fe4fdbf393abf930
aguilarkevin <kevinaguilar0813@gmail.com>
2022-06-12 20:32:10 -0600

    feat: :lipstick: add fonts and base style in site theme

--- themes/site/theme.ts
+++ themes/site/theme.ts
@@ -1,10 +1,12 @@
 import {extendTheme} from '@chakra-ui/react'
 import foundations from './foundations/mod'
 import * as components from './components/mod'
+import styles from './styles'
 
 const overrides = {
   components,
   ...foundations,
+  styles,
 }
 
 export default extendTheme(overrides)
